51af18d33f5b4a4cc382732ab119b8c0562b1a58
Nikita Karetnikov <nikita@karetnikov.org>
2019-07-26 17:45:39 +0300

    Add newtypes for origin and direction

--- src/Main.hs
+++ src/Main.hs
@@ -101,9 +101,12 @@ infixl 6 -.
 minus :: Vec3f -> Vec3f
 minus v = v *.. -1
 
+newtype Orig = Orig { _fromOrig :: Vec3f }
+newtype Dir  = Dir  { _fromDir  :: Vec3f }
+
 -- http://web.archive.org/web/20170707080450/http://www.lighthouse3d.com/tutorials/maths/ray-sphere-intersection/
-rayIntersect :: Sphere -> Vec3f -> Vec3f -> Float -> (Bool, Float)
-rayIntersect (Sphere center radius _) orig dir t0
+rayIntersect :: Sphere -> Orig -> Dir -> Float -> (Bool, Float)
+rayIntersect (Sphere center radius _) (Orig orig) (Dir dir) t0
   | d2 > radius * radius = (False, t0)
   | t1 < 0 && t2 < 0     = (False, t2)
   | t1 < 0               = (True, t2)
@@ -155,7 +158,7 @@ refract i n refractiveIndex =
     k = 1 - eta * eta * (1 - cosi' * cosi')
 
 sceneIntersect
-  :: [Sphere] -> Material -> Vec3f -> Vec3f -> Vec3f -> Vec3f
+  :: [Sphere] -> Material -> Orig -> Dir -> Vec3f -> Vec3f
   -> (Bool, Material, Vec3f, Vec3f)
 sceneIntersect spheres material orig dir point normal =
   ((min spheresDist checkerboardDist) < 1000, material'', point'', normal'')
@@ -163,25 +166,25 @@ sceneIntersect spheres material orig dir point normal =
     (spheresDist, material', point', normal')
       = go maxValue spheres material orig dir point normal
 
-    go :: Float -> [Sphere] -> Material -> Vec3f -> Vec3f -> Vec3f -> Vec3f
+    go :: Float -> [Sphere] -> Material -> Orig -> Dir -> Vec3f -> Vec3f
        -> (Float, Material, Vec3f, Vec3f)
     go sd []     m _ _ p n = (sd, m, p, n)
     go sd (s:ss) m o d p n =
       let di                = 0
           (intersects, di') = rayIntersect s o d di
           m'                = _material s
-          p'                = o +. (d *.. di')
+          p'                = (_fromOrig o) +. ((_fromDir d) *.. di')
           n'                = normalize $ (p' -. _center s)
       in if intersects && di' < sd
          then go di' ss m' o d p' n'
          else go sd  ss m  o d p  n
 
     (checkerboardDist, material'', point'', normal'') =
-      if abs (_v3f_y dir) > 1e-3
+      if abs (_v3f_y (_fromDir dir)) > 1e-3
       then
         -- Checkerboard plane equation: 'y = -4'.
-        let d = -(_v3f_y orig + 4) / (_v3f_y dir)
-            p@(Vec3f x _ z) = orig +. (dir *.. d)
+        let d = -(_v3f_y (_fromOrig orig) + 4) / (_v3f_y $ _fromDir dir)
+            p@(Vec3f x _ z) = (_fromOrig orig) +. ((_fromDir dir) *.. d)
         in if (d > 0 && abs x < 10 && z < -10 && z > -30 && d < spheresDist)
            then
              let n = Vec3f 0 1 0
@@ -195,7 +198,7 @@ sceneIntersect spheres material orig dir point normal =
            else (maxValue, material', point', normal')
       else (maxValue, material', point', normal')
 
-castRay :: [Sphere] -> [Light] -> Int -> Vec3f -> Vec3f -> Vec3f
+castRay :: [Sphere] -> [Light] -> Int -> Orig -> Dir -> Vec3f
 castRay spheres lights depth orig dir =
   if depth > maxDepth || not intersects
   then Vec3f 0.2 0.7 0.8  -- background color
@@ -222,17 +225,19 @@ castRay spheres lights depth orig dir =
       point :: Vec3f
       point = Vec3f 0 0 0
 
-      reflectDir = normalize $ reflect dir n'
-      refractDir = normalize $ refract dir n' (_refractiveIndex material')
+      reflectDir = Dir $ normalize $ reflect (_fromDir dir) n'
+      refractDir = Dir $ normalize $ refract (_fromDir dir) n' (_refractiveIndex material')
 
       -- Offset the original point to avoid occlusion by the object itself.
-      reflectOrig = if (reflectDir *. n') < 0
-                    then point' -. (n' *.. 1e-3)
-                    else point' +. (n' *.. 1e-3)
+      reflectOrig = Orig $
+        if (_fromDir reflectDir *. n') < 0
+        then point' -. (n' *.. 1e-3)
+        else point' +. (n' *.. 1e-3)
 
-      refractOrig = if (refractDir *. n') < 0
-                    then point' -. (n' *.. 1e-3)
-                    else point' +. (n' *.. 1e-3)
+      refractOrig = Orig $
+        if (_fromDir refractDir *. n') < 0
+        then point' -. (n' *.. 1e-3)
+        else point' +. (n' *.. 1e-3)
 
       reflectColor = castRay spheres lights (depth + 1) reflectOrig reflectDir
       refractColor = castRay spheres lights (depth + 1) refractOrig refractDir
@@ -246,13 +251,14 @@ castRay spheres lights depth orig dir =
       go :: Float -> Float -> [Light] -> (Float, Float)
       go dli sli []     = (dli, sli)
       go dli sli (l:ls) =
-        let lightDir      = normalize $ (_position l) -. point'
+        let lightDir      = Dir $ normalize $ (_position l) -. point'
             lightDistance = norm      $ (_position l) -. point'
 
             -- Check if a point lies in the shadow of a light.
-            shadowOrig = if (lightDir *. n') < 0
-                         then point' -. (n' *.. 1e-3)
-                         else point' +. (n' *.. 1e-3)
+            shadowOrig = Orig $
+              if (_fromDir lightDir *. n') < 0
+              then point' -. (n' *.. 1e-3)
+              else point' +. (n' *.. 1e-3)
 
             shadowPoint = Vec3f 0 0 0
             shadowN     = Vec3f 0 0 0
@@ -261,12 +267,12 @@ castRay spheres lights depth orig dir =
             (intersects', _, shadowPoint', _)
               = sceneIntersect spheres tmpMaterial shadowOrig lightDir shadowPoint shadowN
 
-            refl = minus $ reflect (minus lightDir) n'
-            dli' = dli + (_intensity l) * (max 0 (lightDir *. n'))
-            sli' = sli + ((max 0 (refl *. dir)) ** (_specularExponent material'))
+            refl = minus $ reflect (minus $ _fromDir lightDir) n'
+            dli' = dli + (_intensity l) * (max 0 (_fromDir lightDir *. n'))
+            sli' = sli + ((max 0 (refl *. _fromDir dir)) ** (_specularExponent material'))
                  * _intensity l
 
-        in if intersects' && norm (shadowPoint' -. shadowOrig) < lightDistance
+        in if intersects' && norm (shadowPoint' -. _fromOrig shadowOrig) < lightDistance
            then go dli  sli  ls
            else go dli' sli' ls
 
@@ -302,8 +308,8 @@ render file width height spheres lights =
               x     =  (2 * (fi + 0.5) / fWidth  - 1) * tan (fov / 2) * fWidth / fHeight
               y     = -(2 * (fj + 0.5) / fHeight - 1) * tan (fov / 2)
               depth = 0
-              orig  = Vec3f 0 0 0
-              dir   = normalize $ Vec3f x y -1
+              orig  = Orig $ Vec3f 0 0 0
+              dir   = Dir $ normalize $ Vec3f x y -1
           in castRay spheres lights depth orig dir
 
     ppmMaxVal :: Int
